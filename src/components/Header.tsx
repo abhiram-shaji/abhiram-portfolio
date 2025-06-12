dbc024aa1e412360654f47c4f258a62124c2b752
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-06-12 02:20:53 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 const menuItems = [
   { label: 'Home', href: '/' },
   { label: 'Projects', href: '/projects' },
-  { label: 'Articles', href: '/articles' },
+  { label: 'Gists', href: '/Gists' },
   { label: 'Contact', href: '/contact' },
 ];
 
