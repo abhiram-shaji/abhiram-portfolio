de91e3d5dad48e5e5bd826ffeec8c9a6685b641f
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-06-12 17:00:49 -0700

    Update socials.tsx

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -21,51 +21,49 @@ const socials = {
 
 export default function Socials() {
   return (
-    <div className="text-left">
-      <div className="flex gap-6 mt-4">
-        <a href={socials.github} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faGithub}
-            size="2x"
-            className="text-neutral-600 hover:text-[#6e5494] transition-colors duration-200"
-          />
-        </a>
-        <a href={socials.gist} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faGithubAlt}
-            size="2x"
-            className="text-neutral-600 hover:text-[#6e5494] transition-colors duration-200"
-          />
-        </a>
-        <a href={socials.linkedin} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            size="2x"
-            className="text-neutral-600 hover:text-[#0a66c2] transition-colors duration-200"
-          />
-        </a>
-        <a href={`mailto:${socials.email}`} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            size="2x"
-            className="text-neutral-600 hover:text-[#c24040] transition-colors duration-200"
-          />
-        </a>
-        <a href={socials.pexels} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faCamera}
-            size="2x"
-            className="text-neutral-600 hover:text-[#138a72] transition-colors duration-200"
-          />
-        </a>
-        <a href={socials.instagram} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon
-            icon={faInstagram}
-            size="2x"
-            className="text-neutral-600 hover:text-[#e1306c] transition-colors duration-200"
-          />
-        </a>
-      </div>
+    <div className="w-full flex justify-center md:justify-start mt-4 gap-6">
+      <a href={socials.github} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faGithub}
+          size="2x"
+          className="text-neutral-600 hover:text-[#6e5494] transition-colors duration-200"
+        />
+      </a>
+      <a href={socials.gist} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faGithubAlt}
+          size="2x"
+          className="text-neutral-600 hover:text-[#6e5494] transition-colors duration-200"
+        />
+      </a>
+      <a href={socials.linkedin} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          size="2x"
+          className="text-neutral-600 hover:text-[#0a66c2] transition-colors duration-200"
+        />
+      </a>
+      <a href={`mailto:${socials.email}`} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          size="2x"
+          className="text-neutral-600 hover:text-[#c24040] transition-colors duration-200"
+        />
+      </a>
+      <a href={socials.pexels} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faCamera}
+          size="2x"
+          className="text-neutral-600 hover:text-[#138a72] transition-colors duration-200"
+        />
+      </a>
+      <a href={socials.instagram} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faInstagram}
+          size="2x"
+          className="text-neutral-600 hover:text-[#e1306c] transition-colors duration-200"
+        />
+      </a>
     </div>
   );
 }
